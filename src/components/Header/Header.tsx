a209e73fae8a144652d695a44e708bba8d4d89c0
Marianvsf <marianvsf@gmail.com>
2025-07-20 17:04:01 -0400

    Incio home

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import HeaderContent from "./HeaderContent";
+
+const Header = () => {
+    return (
+        <header>
+            <HeaderContent />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Header/HeaderContent.tsx
@@ -0,0 +1,17 @@
+
+import { useRouter } from "next/navigation";
+
+const HeaderContent = () => {
+    const router = useRouter();
+
+return (
+<div>
+    <header>
+    <h1>Hola Mundo</h1>
+    </header>
+</div>
+);
+
+};
+
+export default HeaderContent;
